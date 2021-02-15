31ab2a46bd80cdf52cbd2a5686fd80922c27bea0
swayamrana <swayamr2@illinois.edu>
2021-02-15 15:35:09 -0600

    Completed AdventureDesign class, which is the basic layout of the Adventure, containing the array of rooms with all variables, the starting room, and the ending room

---
+++ src/main/java/student/adventure/AdventureDesign.java
@@ -0,0 +1,40 @@
+package student.adventure;
+
+public class AdventureDesign {
+
+    //the full list of rooms in the JSON file
+    public Room[] rooms;
+    //the name of the room that one starts in, stored in a String
+    public String startingRoom;
+    //the name of the room that one ends in, stored in a String
+    public String endingRoom;
+
+    //Empty constructor for Jackson Mapping
+    public AdventureDesign() {
+
+    }
+
+    /**
+     * getter to retrieve the starting room of the adventure
+     * @return startingRoom
+     */
+    public String getStartingRoom() {
+        return startingRoom;
+    }
+
+    /**
+     * getter to retrieve the ending room of the adventure
+     * @return endingRoom
+     */
+    public String getEndingRoom() {
+        return endingRoom;
+    }
+
+    /**
+     * getter to retrieve the full list of rooms in JSON, all stored in the Room object
+     * @return rooms
+     */
+    public Room[] getRooms() {
+        return rooms;
+    }
+}
